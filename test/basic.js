f9dbdd06ea595f8967a0c49e0da78022c36b4912
Oleksii Rudenko <oleksii.rudenko@n-fuse.de>
2015-10-30 12:10:05 +0100

    Updated tests

--- test/basic.js
+++ test/basic.js
@@ -1,17 +1,16 @@
 var link = require('../lib/link');
-var chai = require('chai');
-chai.use(require('chai-fs'));
-var expect = chai.expect;
+var assert = require('assert');
+var fs = require('fs');
 
 describe('npm-link-shared', function() {
   this.timeout(15000);
   it('should install dependencies via linking', function(done) {
     var base = process.cwd();
     link(base + '/test/shared_modules/', base + '/test/target', []);
-    expect(base + '/test/target/node_modules/module-a').to.be.a.path('module-a does not exist');
-    expect(base + '/test/target/node_modules/module-b').to.be.a.path('module-b does not exist');
-    expect(base + '/test/target/node_modules/module-b/node_modules/lodash').to.be.a.path('lodash does not exist');
-    expect(base + '/test/target/node_modules/module-b/node_modules/chai').not.to.be.a.path('lodash does not exist');
+    assert(fs.existsSync(base + '/test/target/node_modules/module-a'), 'module-a does not exist');
+    assert(fs.existsSync(base + '/test/target/node_modules/module-b'), 'module-b does not exist');
+    assert(fs.existsSync(base + '/test/target/node_modules/module-b/node_modules/lodash'), 'lodash does not exist');
+    assert(!fs.existsSync(base + '/test/target/node_modules/module-b/node_modules/chai'), 'lodash does not exist');
     done();
   });
 });
