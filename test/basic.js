77bf33f51aee1bb4948635d008160ae9fcc35e2b
Sascha Marcel Schmidt <mail@saschaschmidt.net>
2016-07-01 15:05:01 +0200

    adds --production option to tests that call link function to keep the same behaviour as if using hardcoded options

--- test/basic.js
+++ test/basic.js
@@ -7,20 +7,29 @@ describe('npm-link-shared', function() {
 
   it('should install dependencies via linking', function(done) {
     var base = process.cwd();
-    link(base + '/test/shared_modules/', base + '/test/target', []);
+    link(base + '/test/shared_modules/', base + '/test/target', [], [ '--production' ]);
     assert(fs.existsSync(base + '/test/target/node_modules/module-a'), 'module-a does not exist');
     assert(fs.existsSync(base + '/test/target/node_modules/module-b'), 'module-b does not exist');
     assert(fs.existsSync(base + '/test/target/node_modules/module-c'), 'module-c does not exist');
     assert(fs.existsSync(base + '/test/target/node_modules/@scope/module-d'), '@scope/module-d does not exist');
-    assert(fs.existsSync(base + '/test/target/node_modules/module-b/node_modules/lodash'), 'lodash does not exist');
-    assert(!fs.existsSync(base + '/test/target/node_modules/module-b/node_modules/chai'), 'lodash does not exist');
-    assert(fs.existsSync(base + '/test/target/node_modules/module-c/node_modules/lodash'), 'lodash does not exist');
+    assert(
+        fs.existsSync(base + '/test/target/node_modules/module-b/node_modules/lodash'),
+        'lodash does not exist in module-b'
+    );
+    assert(
+        !fs.existsSync(base + '/test/target/node_modules/module-b/node_modules/chai'),
+        'chai does exist'
+    );
+    assert(
+        fs.existsSync(base + '/test/target/node_modules/module-c/node_modules/lodash'),
+        'lodash does not exist in module-c'
+    );
     done();
   });
 
   it('should install dependencies via linking and respect restrictions on modules', function(done) {
     var base = process.cwd();
-    link(base + '/test/shared_modules/', base + '/test/target_single', ['module-c']);
+    link(base + '/test/shared_modules/', base + '/test/target_single', ['module-c'], [ '--production' ]);
     assert(fs.existsSync(base + '/test/target_single/node_modules/module-c'), 'module-c does not exist');
     done();
   });
