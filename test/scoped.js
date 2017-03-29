6ef01c631d86403c4d82950e79420a039257cc60
OliverUv <oliver.uvman@gmail.com>
2017-03-29 19:40:19 +0800

    Refactor api function arguments

--- test/scoped.js
+++ test/scoped.js
@@ -6,7 +6,12 @@ describe('scoped-modules', function() {
   this.timeout(30000);
   it('should install dependencies via linking and respect restrictions on modules', function(done) {
     var base = process.cwd();
-    link(base + '/test/shared_modules/', base + '/test/target_single', [ '@scope/module-d' ], [ '--production' ]);
+    var target = base + '/test/target_single';
+    link(base + '/test/shared_modules/', target, {
+      'moduleWhiteList': ['@scope/module-d'],
+      'linkOptions': ['--production'],
+      'includeDev': true
+    });
     assert(fs.existsSync(base + '/test/target_single/node_modules/@scope/module-d'), '@scope/module-d does not exist');
     done();
   });
